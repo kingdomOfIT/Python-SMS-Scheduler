271655ef9c376a2211875b32d4d2ead2b418f15d
Amir Kahriman <amirkahriman@192.168.1.2>
2024-01-21 09:27:02 +0100

    sms scheduler and simple ui

--- app.py
+++ app.py
@@ -1,10 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
+from datetime import datetime
+from messageService.sms import SMS
 
 app = Flask(__name__)
+app.secret_key = 'tajnaa'
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/sms', methods=['GET', 'POST'])
+def sms():
+    if request.method == 'POST':
+        # Access form data
+        message_body = request.form['messageBody']
+        phone_number = request.form['phoneNumber']
+        date_time = request.form['dateTime']
+
+        input_datetime = datetime.strptime(date_time, "%Y-%m-%dT%H:%M")
+
+        # Extract individual components
+        year = input_datetime.year
+        month = input_datetime.month
+        day = input_datetime.day
+        hour = input_datetime.hour
+        minute = input_datetime.minute
+        second = input_datetime.second
+
+        # Create the desired format for Twilio
+        twilio_format = datetime(year, month, day, hour, minute, second)
+
+        # Send message using twilio rest
+        SMS.send_message(body= message_body, target_number=phone_number, scheduleTime=twilio_format)
+
+        # Flash a success message
+        flash('Message scheduled successfully!', 'success')
+
+        # Redirect to the same page to avoid form resubmission on page refresh
+        return redirect(url_for('sms'))
+
+    return render_template('sms.html')
+
+@app.route('/gmail')
+def gmail():
+    return render_template('gmail.html')
+
+@app.route('/whatsapp')
+def whatsapp():
+    return render_template('whatsapp.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- messageService/sms.py
+++ messageService/sms.py
@@ -1,15 +1,27 @@
 from twilio.rest import Client
-import keys as keys
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# Access the environment variables
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+twilio_number = os.environ['TWILIO_NUMBER']
+messaging_service_sid = os.environ['MESSAGING_SERVICE_ID']
+
 
 class SMS:
-    client = Client(keys.account_id, keys.auth_token)
+    client = Client(account_sid, auth_token)
 
-    def send_message(body, target_number):
+    def send_message(body, target_number, scheduleTime):
 
         message = SMS.client.messages.create(
             body=body,
-            from_=keys.twilio_number,
-            to=target_number
+            from_=twilio_number,
+            to=target_number,
+            messaging_service_sid=messaging_service_sid,
+            send_at=scheduleTime,
+            schedule_type='fixed'
         )
-
-        print(message.body)
+        print(message)
